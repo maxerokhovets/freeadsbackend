e51746c0a419ac5a1dacca11a1f54e50df951a10
maxerokhovets <maxerokhovets@gmail.com>
2021-06-20 20:00:37 +0300

    Initial commit

---
+++ freeads/src/main/java/com/nucldev/freeads/FreeadsApplication.java
@@ -0,0 +1,13 @@
+package com.nucldev.freeads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeadsApplication.class, args);
+	}
+
+}
---
+++ freeads/src/test/java/com/nucldev/freeads/FreeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucldev.freeads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
